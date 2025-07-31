779ab20cb696239b8ebf9e1989e97406a75cc05b
kevinocotan <kaocotan@gmail.com>
2025-07-31 17:09:05 -0600

    Trabajando con el Listado de Pacientes

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,13 @@
+import type { Patient } from "../types"
+
+type PatientDetailsProps = {
+    patient : Patient
+}
+
+export default function PatientDetails({patient} : PatientDetailsProps) {
+  return (
+    <div>
+        {patient.name}
+    </div>
+  )
+}
